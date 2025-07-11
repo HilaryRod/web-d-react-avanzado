da16125f34ad772bbc4eec553ced8901b8ecae32
Hilary <hilaryvirginia49@gmail.com>
2025-07-10 19:10:58 -0600

    esquema de validacion yuo

--- src/components/BasicForm.jsx
+++ src/components/BasicForm.jsx
@@ -1,8 +1,26 @@
+import { yupResolver } from '@hookform/resolvers/yup/src/yup.js'
 import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+
+const schema = yup.object({
+  username: yup
+    .string()
+    .required('El usuario es obligatorio'),
+  password: yup
+    .string()
+    .min(6, 'Password no válido se necesitan 6 caracteres').required('El password es obligatorio'),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref('password')], 'Las contrasenas no coinciden')
+    .required('Confirma tu contrasena')
+})
 
 const BasicForm = () => {
-  const { register, handleSubmit } = useForm()
-
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid }
+  } = useForm()
   const onSubmit = (data) => {
     console.log(data)
   }
@@ -11,6 +29,7 @@ const BasicForm = () => {
     <form onSubmit={handleSubmit(onSubmit)}>
       <input type='text' {...register('username')} placeholder='Usuario' />
       <input type='password' {...register('password')} placeholder='Contrasena' />
+      <input type='password' {...register('confirmPassword')} placeholder='Confirmar Contrasena' />
       <button type='submit'>Enviar</button>
     </form>
   )
