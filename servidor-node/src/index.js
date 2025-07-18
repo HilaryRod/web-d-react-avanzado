a48becad52417c12dfba817ac4d7f7e0ac9b38e3
Hilary <hilaryvirginia49@gmail.com>
2025-07-17 20:06:19 -0600

    se agrego el servidor-node

---
+++ servidor-node/src/index.js
@@ -0,0 +1,11 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('Hola mundo desde node.js')
+})
+
+const PORT = 3000
+server.listen(PORT, () => {
+  console.log('Servidor ejecutandose en el port http://localhost:3000')
+})
