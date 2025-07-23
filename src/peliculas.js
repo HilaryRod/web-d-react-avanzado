78cf11db11805e6f4b53d7b477e4064c2da6de7a
Hilary <hilaryvirginia49@gmail.com>
2025-07-22 21:09:14 -0600

    clase servidor express

---
+++ src/peliculas.js
@@ -0,0 +1,142 @@
+const infoPeliculas = {
+  accion: [
+    {
+      id: 1,
+      titulo: 'John Wick 4',
+      year: 2023,
+      pais: 'usa'
+    },
+    {
+      id: 2,
+      titulo: 'The Batman',
+      year: 2022,
+      pais: 'usa'
+    },
+    {
+      id: 3,
+      titulo: 'Extraction 2',
+      year: 2023,
+      pais: 'usa'
+    },
+    {
+      id: 4,
+      titulo: 'Jurassic World: Dominion',
+      year: 2022,
+      pais: 'usa'
+    },
+    {
+      id: 5,
+      titulo: 'Top Gun: Maverick',
+      year: 2022,
+      pais: 'usa'
+    },
+    {
+      id: 6,
+      titulo: 'Black Panther: Wakanda Forever',
+      year: 2022,
+      pais: 'usa'
+    }
+  ],
+  comedia: [
+    {
+      id: 1,
+      titulo: 'No Hard Feelings',
+      year: 2023,
+      pais: 'usa'
+    },
+    {
+      id: 2,
+      titulo: 'Barbie',
+      year: 2023,
+      pais: 'usa'
+    },
+    {
+      id: 3,
+      titulo: 'Asteroid City',
+      year: 2023,
+      pais: 'usa'
+    },
+    {
+      id: 4,
+      titulo: 'Shotgun Wedding',
+      year: 2022,
+      pais: 'usa'
+    },
+    {
+      id: 5,
+      titulo: 'The Lost City',
+      year: 2022,
+      pais: 'usa'
+    },
+    {
+      id: 6,
+      titulo: 'Ticket to Paradise',
+      year: 2022,
+      pais: 'usa'
+    }
+  ],
+  terror: [
+    {
+      id: 1,
+      titulo: 'M3GAN',
+      year: 2023,
+      pais: 'usa'
+    },
+    {
+      id: 2,
+      titulo: 'Scream VI',
+      year: 2023,
+      pais: 'usa'
+    },
+    {
+      id: 3,
+      titulo: 'Sé lo que hicieron el verano pasado',
+      year: 2021,
+      pais: 'usa'
+    },
+    {
+      id: 4,
+      titulo: 'The Black Phone',
+      year: 2022,
+      pais: 'usa'
+    },
+    {
+      id: 5,
+      titulo: 'Smile',
+      year: 2022,
+      pais: 'usa'
+    },
+    {
+      id: 6,
+      titulo: 'Evil Dead Rise',
+      year: 2023,
+      pais: 'usa'
+    },
+    {
+      id: 7,
+      titulo: 'Talk to Me',
+      year: 2023,
+      pais: 'australia'
+    },
+    {
+      id: 8,
+      titulo: 'The Nun II',
+      year: 2023,
+      pais: 'usa'
+    },
+    {
+      id: 9,
+      titulo: 'Insidious: The Red Door',
+      year: 2023,
+      pais: 'usa'
+    },
+    {
+      id: 10,
+      titulo: 'A Quiet Place: Day One',
+      year: 2024,
+      pais: 'usa'
+    }
+  ]
+}
+
+module.exports.infoPeliculas = infoPeliculas
